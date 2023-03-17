bffb08393516eef3fca24d7e7661c1dbca4920cf
Akshit Mokani <46166207+AkshitMokani@users.noreply.github.com>
2023-03-17 11:36:18 +0530

    Update 1Counter.sol

--- Week1 Task/1Counter.sol
+++ Week1 Task/1Counter.sol
@@ -5,8 +5,7 @@ pragma solidity ^0.8.0;
 contract counter
 {
     uint public count = 4;
-    uint newValue;
-    //event incrementc(string str,uint count,string str1,uint newValue);
+    uint newValue;  
     event incrementc(uint oldVlaue,uint newValue);
 
     function increment() public returns(uint)
@@ -15,21 +14,4 @@ contract counter
         emit incrementc(count-5,count);
         return count;
     }
-
-
-    //event decrementc(uint newCount);
-
-    // function Increment() public returns(uint)
-    // {
-    //     newValue = count + 5;
-    //     emit incrementc("Old Value is : ",count,"New Value is:",newValue);
-    //     return count;
-    // }
-
-    // function Decrement() public returns(uint)
-    // {
-    //     count = count - 2;
-    //     emit incrementc("Old Value is : ",count,"New Value is:",newValue);
-    //     return count;
-    // }
 }
